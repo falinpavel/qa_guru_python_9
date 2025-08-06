6cb546970027e19dbf9654997b26b379901a948e
Falin Pavel <falinpavel96@gmail.com>
2025-08-06 17:01:00 +0300

    high level steps object is done

--- data/user_info.py
+++ data/user_info.py
@@ -14,13 +14,19 @@ class User:
     user_email: str
     gender: str
     user_number: str
+    birth_day: str
+    birth_month: str
+    birth_year: str
     subjects: list
     hobbies: list
     current_address: str
+    state: str
+    city: str
 
     def __hash__(self):
-        return hash((self.first_name, self.last_name, self.user_email,
-                     self.gender, self.user_number, self.current_address))
+        return hash((self.first_name, self.last_name, self.user_email, self.gender, self.user_number, self.birth_day,
+                     self.birth_month, self.birth_year, self.subjects, self.hobbies, self.current_address, self.state,
+                     self.city))
 
 
 class UsersForTests:
@@ -38,13 +44,22 @@ class UsersForTests:
                      user_email=fake.unique.email(),
                      gender=fake.random_elements(elements=('Male', 'Female', 'Other'), length=1)[0],
                      user_number="8" + "".join([str(random.randint(0, 9)) for _ in range(9)]),
+                     birth_day=str(random.randint(a=4, b=28)),
+                     birth_month=fake.random_elements(elements=('January', 'February', 'March', 'April', 'May',
+                                                                'June', 'July', 'August', 'September', 'October',
+                                                                'November', 'December'), length=1)[0],
+                     birth_year=str(fake.year()),
                      subjects=list(fake.random_elements(elements=('Maths', 'English', 'Computer Science',
                                                                   'Chemistry', 'Physics', 'Biology', 'Accounting',
                                                                   'Arts', 'Commerce', 'Economics', 'History'),
-                                                        length=random.randint(a=1, b=4))),
+                                                        length=random.randint(a=2, b=4))),
                      hobbies=list(fake.random_elements(elements=('Sports', 'Music', 'Reading'),
                                                        length=random.randint(a=1, b=2))),
-                     current_address=fake.address())
+                     current_address=fake.address(),
+                     # state=fake.random_elements(elements=('NCR', 'Uttar Pradesh',
+                     # 'Haryana', 'Rajasthan'), length=1)[0], TODO fix
+                     state='Uttar Pradesh',
+                     city=fake.random_elements(elements=('Agra', 'Lucknow', 'Merrut'), length=1)[0])
             users.append(_)
         return users
 
--- pages/forms/practice_form/page_practice_form.py
+++ pages/forms/practice_form/page_practice_form.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from selene import browser, be, have
 from const import UPLOADED_FILE
 from data.user_info import UsersForTests as Users
@@ -8,6 +9,7 @@ class PracticeFormPage:
     URL = '/automation-practice-form'
 
     user = Users().choose_random_user()
+    print(user)
 
     def open_page(self) -> browser:
         browser.open(self.URL)
@@ -15,41 +17,34 @@ class PracticeFormPage:
     def register_user_male_and_submit_form(self) -> None:
         browser.element('#firstName').should(be.blank).type(self.user.first_name).should(be.not_.blank).should(
             have.attribute("value").value(self.user.first_name))
-
         browser.element('#lastName').should(be.blank).type(self.user.last_name).should(be.not_.blank).should(
             have.attribute("value").value(self.user.last_name))
-
         browser.element('#userEmail').should(be.blank).type(self.user.user_email).should(be.not_.blank).should(
             have.attribute("value").value(self.user.user_email))
-
         browser.all('[class="custom-control-label"]').element_by(
             have.text(self.user.gender)).click().should(be.enabled)
-
         browser.element('#userNumber').should(be.blank).send_keys(self.user.user_number).should(
             be.not_.blank).should(have.attribute("value").value(self.user.user_number))
-
         browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__month-select').click().element('option[value="4"]').click()
-        browser.element('.react-datepicker__year-select').click().element('option[value="1996"]').click()
-        browser.element('div[aria-label="Choose Thursday, May 23rd, 1996"]').click()
-        browser.element('#dateOfBirthInput').should(be.not_.blank).should(have.attribute("value").value('23 May 1996'))
-
+        browser.element('.react-datepicker__month-select').click().all('[value]').element_by(
+            have.text(self.user.birth_month)).click()
+        browser.element('.react-datepicker__year-select').click().all('[value]').element_by(
+            have.attribute("value").value(self.user.birth_year)).click()
+        browser.element('.react-datepicker__month').click().all('[role="option"]').element_by(
+            have.text(self.user.birth_day)).click()
+        browser.element('#dateOfBirthInput').should(be.not_.blank).should(have.attribute("value").value(
+            f'{int(self.user.birth_day):02d} {self.user.birth_month[:3]} {self.user.birth_year}'))
         for subject in self.user.subjects:
             browser.element('#subjectsInput').type(subject).press_enter()
-
         for hobby in self.user.hobbies:
             browser.all('label[class="custom-control-label"]').element_by(
                 have.text(hobby)).click().should(be.enabled)
-
         browser.element('#uploadPicture').send_keys(UPLOADED_FILE)
-
         browser.element('#currentAddress').should(be.blank).type(self.user.current_address).should(
             be.not_.blank).should(have.attribute("value").value(self.user.current_address))
-
-        browser.element('#state').click().element('#react-select-3-option-1').click()
-        browser.element('#city').click().element('#react-select-4-option-1').click()
-
-        browser.element('#submit').click()
+        browser.element('#state').click().all('[tabindex="-1"]').element_by(have.text(self.user.state)).click()
+        browser.element('#city').click().all('[tabindex="-1"]').element_by(have.text(self.user.city)).click()
+        browser.element('#submit').should(be.clickable).click()
 
     def should_table_be_filled(self) -> None:
         table_element = browser.all('table.table-dark tbody tr')
@@ -59,7 +54,8 @@ class PracticeFormPage:
             have.text(self.user.user_email))
         table_element.element_by(have.text('Gender')).all('td').second.should(have.text(self.user.gender))
         table_element.element_by(have.text('Mobile')).all('td').second.should(have.text(self.user.user_number))
-        table_element.element_by(have.text('Date of Birth')).all('td').second.should(have.text("23 May,1996"))
+        table_element.element_by(have.text('Date of Birth')).all('td').second.should(
+            have.text(f'{self.user.birth_day} {self.user.birth_month},{self.user.birth_year}'))
         table_element.element_by(have.text('Subjects')).all('td').second.should(
             have.text(', '.join(self.user.subjects)))
         table_element.element_by(have.text('Hobbies')).all('td').second.should(
@@ -68,48 +64,31 @@ class PracticeFormPage:
         table_element.element_by(have.text('Address')).all('td').second.should(
             have.text(self.user.current_address))
         table_element.element_by(have.text('State and City')).all('td').second.should(
-            have.text("Uttar Pradesh Lucknow"))
+            have.text(f"{self.user.state} {self.user.city}"))
 
-    # @staticmethod
-    # def should_all_texts_into_form(
-    #         center_text: str,
-    #         form_text_label: str,
-    #         name_text_label: str,
-    #         first_name_placeholder: str,
-    #         last_name_placeholder: str,
-    #         gender_text_label: str,
-    #         gender_text_male: str,
-    #         gender_text_female: str,
-    #         gender_text_other: str,
-    #         number_text_label: str,
-    #         number_text_small: str,
-    #         number_placeholder: str,
-    #         birthday_text_label: str,
-    #         subjects_text_label: str,
-    #         hobbies_text_label: str,
-    #         hobbies_text_sport: str,
-    #         hobbies_text_reed: str,
-    #         hobbies_text_music: str,
-    #         address_text_label: str,
-    #         address_placeholder: str,
-    #         state_city_text_label: str
-    # ):
-    #     browser.element('.text-center').should(have.text(center_text))
-    #     browser.element('.practice-form-wrapper h5').should(have.text(form_text_label))
-    #     browser.element('#userName-wrapper').should(have.exact_text(name_text_label))
-    #     browser.element('#firstName').should(have.attribute('placeholder').value(first_name_placeholder))
-    #     browser.element('#lastName').should(have.attribute('placeholder').value(last_name_placeholder))
-    #     browser.element('#genterWrapper').should(have.text(gender_text_label))
-    #     browser.all('.custom-radio').should(have.size(3)).should(
-    #         have.exact_texts(gender_text_male, gender_text_female, gender_text_other))
-    #     browser.element('#userNumber-label').should(have.text(number_text_label)).element('small').should(
-    #         have.text(number_text_small))
-    #     browser.element('#userNumber').should(have.attribute('placeholder').value(number_placeholder))
-    #     browser.element('#dateOfBirth-label').should(have.text(birthday_text_label))
-    #     browser.element('#subjectsWrapper').should(have.text(subjects_text_label))
-    #     browser.element('#hobbiesWrapper').should(have.text(hobbies_text_label))
-    #     browser.all('.custom-checkbox').should(have.size(3)).should(
-    #         have.exact_texts(hobbies_text_sport, hobbies_text_reed, hobbies_text_music))
-    #     browser.element('#currentAddress-wrapper').should(have.text(address_text_label))
-    #     browser.element('#currentAddress').should(have.attribute('placeholder').value(address_placeholder))
-    #     browser.element('#stateCity-wrapper').should(have.text(state_city_text_label))
+    @staticmethod
+    def should_all_texts_into_form() -> None:
+        browser.element('.text-center').should(have.text('Practice Form'))
+        browser.element('.practice-form-wrapper h5').should(have.text('Student Registration Form'))
+        browser.element('#userName-wrapper').should(have.exact_text('Name'))
+        browser.element('#firstName').should(have.attribute('placeholder').value('First Name'))
+        browser.element('#lastName').should(have.attribute('placeholder').value('Last Name'))
+        browser.element('#genterWrapper').should(have.text('Gender'))
+        browser.all('.custom-radio').should(have.size(3)).should(have.exact_texts('Male', 'Female', 'Other'))
+        browser.element('#userNumber-label').should(have.text('Mobile')).element('small').should(
+            have.text('(10 Digits)'))
+        browser.element('#userNumber').should(have.attribute('placeholder').value('Mobile Number'))
+        browser.element('#dateOfBirth-label').should(have.text('Date of Birth'))
+        browser.element('#dateOfBirthInput').should(have.attribute('value').value(datetime.now().strftime('%d %b %Y')))
+        browser.element('#subjectsWrapper').should(have.text('Subjects'))
+        browser.element('#hobbiesWrapper').should(have.text('Hobbies'))
+        browser.all('.custom-checkbox').should(have.size(3)).should(have.exact_texts('Sports', 'Reading', 'Music'))
+        browser.element('#currentAddress-wrapper').should(have.text('Current Address'))
+        browser.element('#currentAddress').should(have.attribute('placeholder').value('Current Address'))
+        browser.element('#stateCity-wrapper').should(have.text('State and City'))
+
+    @staticmethod
+    def form_not_filled_and_not_submit() -> None:
+        browser.element('#submit').click()
+        # browser.element('.modal-title').should(have.text('Thanks for submitting the form'))
+        browser.element('.modal-title').should(be.not_.present)
